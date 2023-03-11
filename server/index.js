4090cbde5963d768f93f7aff2a1092601a7bdd89
vlmiovcic <dovlica@web.de>
2023-03-11 15:17:01 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const https = require("https");
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const privKey = fs.readFileSync('sslcert/it-vladimirmiovcic.com.key','utf8');
+const privKey = fs.readFileSync('sslcert/it-vladimirmiovcic.com.key', 'utf8');
 const cert = fs.readFileSync('sslcert/it-vladimirmiovcic.com.crt', 'utf8');
 const process = require('process');
 const PORT = process.env.PORT || 4043;
@@ -9,26 +9,26 @@ const app = require('./routes/general.routes');
 
 
 const options = {
-  key: privKey,
-  cert: cert
+    key: privKey,
+    cert: cert
 };
-
+let test = 1;
+console.log(test);
 
 const dbInquiries = require("./models");
 dbInquiries.sequelize.sync()
     .then(() => {
         console.log('Synced db.');
     })
-    .catch((error) =>{
-       console.error('Sync error: ', error.message);
+    .catch((error) => {
+        console.error('Sync error: ', error.message);
     });
 
 const httpsServer = https.createServer({
-  key: options.key,
-  cert: options.cert,
+    key: options.key,
+    cert: options.cert,
 }, app);
 
 httpsServer.listen(PORT, () => {
     console.log('HTTPS Server running on port ' + PORT);
 });
-
